3707aafd561424214625ecb668a96a675e2a3286
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-11-11 20:40:52 -0300

    Extrai constantes

--- src/Disciplina.jsx
+++ src/Disciplina.jsx
@@ -1,10 +1,14 @@
 import "./Disciplina.css";
 
 function Disciplina() {
+
+    const codigo = "DCC207"
+    const nome = "Desenvolvimento Front-End"
+
   return (
     <div className="disciplina">
-      <h2>DCC207</h2>
-      <h3>Desenvolvimento Front-End</h3>
+      <h2>{codigo}</h2>
+      <h3>{nome}</h3>
     </div>
   );
 }
