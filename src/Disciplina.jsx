38581352c90b47398f4f2f9fd243b3f58199d69a
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2024-11-11 20:37:06 -0300

    Adiciona estilo do componente Disciplina

--- src/Disciplina.jsx
+++ src/Disciplina.jsx
@@ -1,6 +1,8 @@
+import "./Disciplina.css";
+
 function Disciplina() {
   return (
-    <div>
+    <div className="disciplina">
       <h2>DCC207</h2>
       <h3>Desenvolvimento Front-End</h3>
     </div>
